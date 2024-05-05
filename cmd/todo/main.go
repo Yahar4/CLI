7386305270489b9acaa8978fcf7db68b2164cc46
Rendayse <d1ma.jashkov@yandex.ru>
2024-05-05 22:31:06 +0300

    wrote main functions

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,1 +1,38 @@
-package todo
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/Yahar4/CLI"
+	"os"
+)
+
+const (
+	todoFile = ".todos.json"
+)
+
+func main() {
+
+	add := flag.Bool("add", false, "add a new todo")
+
+	flag.Parse()
+
+	todos := &CLI.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+	switch {
+	case *add:
+		todos.Add("Sample todo")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
+}
